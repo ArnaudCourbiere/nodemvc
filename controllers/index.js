32512ef495136836dfee8219a6cfddb4041f7229
Arnaud <arnaud.courbiere@gmail.com>
2012-06-07 09:37:26 -0700

    Typo.

--- controllers/index.js
+++ controllers/index.js
@@ -3,5 +3,5 @@
  */
 
 module.exports.indexAction = function (req, res) {
-    res.send('This is the index action of the index contorller.');
+    res.send('This is the index action of the index controller.');
 }
